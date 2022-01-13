461e81d0b69696b0c373b8cc42b4a4bbaf8847a1
Animila <hristoforovila5@gmail.com>
2022-01-13 23:32:11 +0300

    New Character Control

--- Assets/Script/Character_controller.cs
+++ Assets/Script/Character_controller.cs
@@ -3,25 +3,51 @@ using UnityEngine;
 
 public class Character_controller : MonoBehaviour
 {
-    [SerializeField] private float jump = 20f;
     bool inAir;
     private Rigidbody2D rb;
+    [SerializeField] private int xVelocity = 5;
+    [SerializeField] private int yVelocity = 8;
+    [SerializeField] private bool isBrouser;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
     }
 
-    public void Update() {
-        if (Input.GetKeyDown(KeyCode.Space) && !inAir) 
-        {
-            inAir = true;
-            rb.AddForce(Vector2.up * jump, ForceMode2D.Impulse);
-        }
+    public void Update()
+    {
+        updatePlayerPosition();
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.layer == LayerMask.NameToLayer("Ground"))
-        inAir = false;
+            inAir = false;
+    }
+
+    private void updatePlayerPosition() 
+    {
+        float jumpInput = Input.GetAxis("Jump");
+        if(isBrouser)
+        {
+            //get input
+            float moveInput = Input.GetAxis("Horizontal");
+
+            //left
+            //right
+            //jump
+            if (moveInput < 0) 
+            {
+                rb.velocity = new Vector2(-xVelocity, rb.velocity.y);
+            } 
+            else if (moveInput > 0) 
+            {
+                rb.velocity = new Vector2(xVelocity, rb.velocity.y);
+            }
+        }
+        if (jumpInput > 0 && !inAir)
+        {
+            inAir = true;
+            rb.velocity = new Vector2(rb.velocity.x, yVelocity);
+        }
     }
 }
