215f4e7affbb095cf601c5c90c6be1fe6bd84f66
Animila <hristoforovila5@gmail.com>
2022-01-13 23:13:35 +0300

    Rebuild Jump Script

--- Assets/Script/Character_controller.cs
+++ Assets/Script/Character_controller.cs
@@ -3,19 +3,25 @@ using UnityEngine;
 
 public class Character_controller : MonoBehaviour
 {
-   [SerializeField] private float jump = 20f;
+    [SerializeField] private float jump = 20f;
+    bool inAir;
+    private Rigidbody2D rb;
 
-   private Rigidbody2D rb;
-
-   private void Start()
+    private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
     }
 
-   public void Update() {
-    if (Input.GetKeyDown(KeyCode.Space)) 
-    {
-        rb.AddForce(Vector2.up * jump, ForceMode2D.Impulse);
+    public void Update() {
+        if (Input.GetKeyDown(KeyCode.Space) && !inAir) 
+        {
+            inAir = true;
+            rb.AddForce(Vector2.up * jump, ForceMode2D.Impulse);
+        }
     }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Ground"))
+        inAir = false;
     }
 }
