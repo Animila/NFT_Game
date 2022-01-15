017230f1f9d4fc7db04c461deea4a4329924203e
Animila <hristoforovila5@gmail.com>
2022-01-15 12:27:08 +0300

    Add Script Control Cameras

---
+++ Assets/Script/Camera_control.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_control : MonoBehaviour
+{
+    [Header("Параметры")]
+    [SerializeField] private Transform playerTransform;
+    [SerializeField] private string playerTag;
+    [SerializeField] private float MovingCamera;
+    private void Awake() {
+        if(this.playerTransform == null)
+        {
+            if(this.playerTag == "")
+            {
+                this.playerTag = "Player";
+            }
+
+            this.playerTransform = GameObject.FindGameObjectWithTag(this.playerTag).transform;
+        }
+
+        this.transform.position = new Vector3(){
+            x = this.playerTransform.position.x,
+            y = this.playerTransform.position.y + 2.150523f, 
+            z = this.playerTransform.position.z - 1,
+        };
+    }
+
+    private void Update() {
+        if(this.playerTransform)
+        {
+            Vector3 target = new Vector3()
+            {
+                x = this.playerTransform.position.x,
+                y = this.playerTransform.position.y + 2.150523f,
+                z = this.playerTransform.position.z - 1,
+            };
+
+            Vector3 pos = Vector3.Lerp(this.transform.position, target, this.MovingCamera * Time.deltaTime);
+
+            this.transform.position = pos;
+        }
+    }
+}
