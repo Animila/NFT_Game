01dba0c2cd576b0a5d188e1f102e777c9cbd826f
Animila <hristoforovila5@gmail.com>
2022-01-17 23:00:11 +0300

    Script for character

--- Assets/Script/Character_controller.cs
+++ Assets/Script/Character_controller.cs
@@ -1,69 +1,38 @@
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
-public class Character_controller : MonoBehaviour
+public class Character_Controller : MonoBehaviour
 {
-    [SerializeField] private float jumpForce = 7f;
-    [SerializeField] private float speedForce;
-    [SerializeField] private bool canJump;
-    [SerializeField] private Rigidbody2D rb;
+    public float jumpForce = 5f;
+    public float forwardForce = 0f;
 
-    private void Start()
-    {
+    private Rigidbody2D rb;
+    private bool canJump;
+
+    private void Start() {
         rb = GetComponent<Rigidbody2D>();
     }
 
     private void Update() {
-        if(Input.GetKey(KeyCode.Space)){
-            if(transform.position.x < 0){
-                rb.velocity = new Vector2(1f, transform.position.y);
-            } else {
-                speedForce = 0;
-            }
+        if(Input.GetKeyDown(KeyCode.Space)){
             Jump();
         }
     }
-    public void Jump()
-    {
+
+    public void Jump(){
         if(canJump){
             canJump = false;
-            rb.AddForce(Vector2.up * jumpForce);
+            if(transform.position.x < 0){
+                forwardForce = 1f;
+            } else {
+                forwardForce = 0f;
+            }
+            rb.velocity = new Vector2(forwardForce, jumpForce);
         }
     }
 
-    private void OnCollisionEnter2D(Collision2D other)
-    {
-        if(other.gameObject.CompareTag("Ground") || other.gameObject.CompareTag("Enemy"))
-        {
-            canJump = true;
-        }
+    private void OnCollisionEnter2D(Collision2D other) {
+        canJump = true;
     }
-
-    // public float moveSpeed;
-    // public float jumpForce;
-
-    // private Rigidbody2D rb;
-
-    // public bool groundIn;
-    // public LayerMask whatIsGround;
-    // private Collider2D myCol;
-
-    // private void Start() {
-    //     rb = GetComponent<Rigidbody2D>();
-
-    //     myCol = GetComponent<Collider2D>();
-    // }
-
-    // void Update(){
-    //     groundIn = Physics2D.IsTouchingLayers(myCol, whatIsGround);
-    //     rb.velocity = new Vector2(moveSpeed, rb.velocity.y);
-    //     if(Input.GetKeyDown(KeyCode.Space)){
-    //         Jump();
-    //     }
-    // }
-
-    // public void Jump(){
-    //     rb.velocity = new Vector2(rb.velocity.x, jumpForce);
-    // }
-    
 }
